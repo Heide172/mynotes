76e86059852ef6c3bd9cf5c4d25fee211310c678
Arthur Tonyan <zeberg172@gmail.com>
2023-05-15 21:06:50 +0300

    init commit

---
+++ app/notes/page.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import { supabase } from '../supabaseClient';
+import styles from './Notes.module.css';
+
+async function getNotes() {
+    const { data, error } = await supabase.from('notes').select()
+ //  {cache: "no store"} 
+ // return JSON.stringify({ data }, null, 2)}
+    return data}
+    
+
+
+export default async function NotesPage() {
+    const notes = await getNotes()
+    return(
+        <div>
+            <h1>Notes</h1>
+            <div>
+               {notes?.map((note) => {
+          return <Note key={note.id} note={note} />;
+        })}
+            </div>
+        </div>
+    )
+}
+
+function Note({ note }: any){
+    const {id, created_at, title, content} = note || {}
+    
+    return (
+        <Link href={`/notes/${id}`}>
+          <div className={styles.note}>
+            <h2>{title}</h2>
+            <h5>{content}</h5>
+            <p>{created_at}</p>
+          </div>
+        </Link>
+      )
+}
